1e18621d27972b0c31ea1b065b6e68b14503fbda
Mohamad Rizal Nurochman <muhamadrizalnurohman072005@gmail.com>
2025-10-26 18:48:31 +0700

    Refactor Produk entity and validation struct for consistency in field definitions

--- apps/api/package/dto/produkValidation.go
+++ apps/api/package/dto/produkValidation.go
@@ -2,8 +2,8 @@ package dto
 
 type ProdukCreate struct {
 	Nama_Produk		string		`json:"nama_produk" binding:"required"`
-	Harga_Beli		int		`json:"harga_beli" binding:"required,gt=0"`
-	Harga_Jual		int		`json:"harga_jual" binding:"required,gt=0"`
+	Harga_Beli		int				`json:"harga_beli" binding:"required,gt=0"`
+	Harga_Jual		int				`json:"harga_jual" binding:"required,gt=0"`
 	Stok					int				`json:"stok" binding:"required,gte=0"`
-	ID_Kategori		*uint			`json:"kategori_id"`
+	ID_Kategori		uint			`json:"kategori_id"`
 }
